11e283d69b454551c9a5e59a566c59a413a30841
Bruno Santos <brunosantosengmec@gmail.com>
2020-02-03 21:38:37 -0300

    projeto final

---
+++ js/enviar.js
@@ -0,0 +1,14 @@
+function enviar() {
+
+    var emailValue = document.getElementById('emailTxt').value;
+    var areaValue = document.getElementById('areaTxt').value;
+    var timestamp = new Date().getTime();
+    console.log(emailValue);
+    console.log(areaValue);
+    // A variavel database vai receber as funções de acesso ao banco de dados
+    var database = firebase.database();
+    database.ref(timestamp).set({
+        Email: emailValue,
+        Mensagem: areaValue
+    });
+}
---
+++ js/init.js
@@ -0,0 +1,11 @@
+var firebaseConfig = {
+    apiKey: "AIzaSyDvzcryzP5g0y28M-CDoxMWYcdTMpJ2Rj8",
+    authDomain: "projeto-final-23397.firebaseapp.com",
+    databaseURL: "https://projeto-final-23397.firebaseio.com",
+    projectId: "projeto-final-23397",
+    storageBucket: "projeto-final-23397.appspot.com",
+    messagingSenderId: "55684429377",
+    appId: "1:55684429377:web:6ecd270d1bc4106501b8c0"
+};
+// Initialize Firebase
+firebase.initializeApp(firebaseConfig);
